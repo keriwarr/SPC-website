a6baa24b61406789aa6ddd3b3b4e952ba3eac5c1
AlbertPangilinan <rapangil@edu.uwaterloo.ca>
2018-06-20 19:21:23 -0400

    added views and templates functionality

--- mysite/spc_main/urls.py
+++ mysite/spc_main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('teams/', views.teams, name='teams'),
+    path('users/', views.users, name='users'),
 ]
--- mysite/spc_main/views.py
+++ mysite/spc_main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+from .models import User, Team, Project, Interest
 
 def index(request):
-    return HttpResponse("A surprise to be sure, but a welcome one.")
+    context = {}
+    return render(request, 'spc_main/index.html', context)
+
+def teams(request):
+    return HttpResponse("Teams")
+
+def users(request):
+    return HttpResponse("Users")
