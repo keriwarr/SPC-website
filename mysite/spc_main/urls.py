e196d4f991bb638fbeaa68bc84d15b1331ad2150
AlbertPangilinan <rapangil@edu.uwaterloo.ca>
2018-07-04 18:56:38 -0400

    added view templates for all pages, moved css styling into static folder for django use

--- mysite/spc_main/urls.py
+++ mysite/spc_main/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('faq/', views.faq, name='faq'),
+    path('past_projects/', views.past_projects, name='past_projects'),
+    path('get_involved/', views.get_involved, name='get_involved'),
+    path('partners/', views.partners, name='partners'),
     path('teams/', views.teams, name='teams'),
     path('users/', views.users, name='users'),
 ]
--- mysite/spc_main/views.py
+++ mysite/spc_main/views.py
@@ -6,6 +6,26 @@ def index(request):
     context = {}
     return render(request, 'spc_main/index.html', context)
 
+def about(request):
+    context = {}
+    return render(request, 'spc_main/about.html', context)
+
+def faq(request):
+    context = {}
+    return render(request, 'spc_main/faq.html', context)
+
+def past_projects(request):
+    context = {}
+    return render(request, 'spc_main/past_projects.html', context)
+
+def get_involved(request):
+    context = {}
+    return render(request, 'spc_main/get_involved.html', context)
+
+def partners(request):
+    context = {}
+    return render(request, 'spc_main/partners.html', context)
+
 def teams(request):
     return HttpResponse("Teams")
 
